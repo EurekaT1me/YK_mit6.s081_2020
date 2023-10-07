47034e956d4dcc97bb4af4899dd207fcc635e070
Yikai_Zhang <646190204@qq.com>
2023-10-07 16:19:08 +0800

    finish lazytests and usertests

--- kernel/defs.h
+++ kernel/defs.h
@@ -145,7 +145,8 @@ void            trapinit(void);
 void            trapinithart(void);
 extern struct spinlock tickslock;
 void            usertrapret(void);
-
+int             isPageFault(uint64);
+void            allocMapPage(uint64);
 // uart.c
 void            uartinit(void);
 void            uartintr(void);
@@ -168,6 +169,7 @@ void            uvmfree(pagetable_t, uint64);
 void            uvmunmap(pagetable_t, uint64, uint64, int);
 void            uvmclear(pagetable_t, uint64);
 uint64          walkaddr(pagetable_t, uint64);
+pte_t *         walk(pagetable_t, uint64, int);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -46,10 +46,12 @@ sys_sbrk(void)
 
   if(argint(0, &n) < 0)
     return -1;
-  addr = myproc()->sz;
-  myproc()->sz+=n;
-//  if(growproc(n) < 0)
-//    return -1;
+  struct proc *p=myproc();
+  addr = p->sz;
+  p->sz+=n;
+  if(n<0){
+      uvmdealloc(p->pagetable,addr,p->sz);
+  }
   return addr;
 }
 
