44f04cebc0a8352c8d508250bc73a47f690f81a9
Yikai_Zhang <646190204@qq.com>
2023-10-08 09:13:08 +0800

    start lab: cow

--- kernel/defs.h
+++ kernel/defs.h
@@ -145,8 +145,7 @@ void            trapinit(void);
 void            trapinithart(void);
 extern struct spinlock tickslock;
 void            usertrapret(void);
-int             isHeapPageFault(uint64);
-void            allocMapPage(uint64);
+
 // uart.c
 void            uartinit(void);
 void            uartintr(void);
@@ -169,7 +168,6 @@ void            uvmfree(pagetable_t, uint64);
 void            uvmunmap(pagetable_t, uint64, uint64, int);
 void            uvmclear(pagetable_t, uint64);
 uint64          walkaddr(pagetable_t, uint64);
-pte_t *         walk(pagetable_t, uint64, int);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
